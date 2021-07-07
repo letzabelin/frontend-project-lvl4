1ce518ca9f29a55cfb827dbf78b7647ec760e24c
Andrew Zabelin <a.zabelin@i-t-p.pro>
2021-07-07 17:43:22 +0300

    feat: update bootstrap

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React from "react";
+import {
+  Form,
+  FloatingLabel,
+  Navbar,
+  Container,
+  Card,
+  Row,
+  Col,
+  Button,
+} from "react-bootstrap";
 
-const Login = () => (
-  <div>
-    hello
-  </div>
-);
+const Login = () => {
+  return (
+    <div className="d-flex flex-column h-100">
+      <Navbar className="shadow-sm">
+        <Container>
+          <Navbar.Brand href="#home">Hello</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container fluid="lg">
+        <Row className="justify-content-center align-content-center h-100">
+          <Col>
+            {/* <Form className="p-5"> */}
+            {/*   <FloatingLabel */}
+            {/*     controlId="floatingInput" */}
+            {/*     label="Email address" */}
+            {/*     className="mb-3" */}
+            {/*   > */}
+            {/*     <Form.Control type="email" placeholder="name@example.com" /> */}
+            {/*   </FloatingLabel> */}
+            {/*   <FloatingLabel controlId="floatingPassword" label="Password"> */}
+            {/*     <Form.Control type="password" placeholder="Password" /> */}
+            {/*   </FloatingLabel> */}
+            {/*   <Button type="Submit">Submit</Button> */}
+            {/* </Form> */}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default Login;
