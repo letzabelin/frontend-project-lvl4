3569fe9af0bc0fe4eafbbf671025708c47e7876e
Andrew Zabelin <a.zabelin@i-t-p.pro>
2021-07-09 17:29:09 +0300

    fix: change locales path

--- src/localize.js
+++ src/localize.js
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translationEn from './locales/en/translation.json';
-import translationRu from './locales/ru/translation.json';
+import translationEn from '../locales/en/translation.json';
+import translationRu from '../locales/ru/translation.json';
 
 const resources = {
   ru: {
