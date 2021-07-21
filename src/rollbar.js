587e241b5458dabc6f7e95febb530a77521e00e9
Andrew Zabelin <a.zabelin@i-t-p.pro>
2021-07-21 13:37:41 +0300

    test

--- src/rollbar.js
+++ src/rollbar.js
@@ -2,4 +2,7 @@ export default {
   accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
 };
