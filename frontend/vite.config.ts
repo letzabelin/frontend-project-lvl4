b482b464e63e9ade7f91bd9ed0417a7e08d2141d
Andrew Zabelin <letzabelin@gmail.com>
2023-03-01 18:14:15 +0300

    chore: add @hexlet/chat-server in deps, configure frontend proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [react()],
+    server: {
+        proxy: {
+          '/app': {
+            target: 'http://localhost:5001',
+            channgeOrigin: true,
+            secure: false,
+          },
+        },
+    },
 });
