e432cd9939925a5c634d8a4104f94fc838d28b2c
Andrew Zabelin <letzabelin@gmail.com>
2023-03-02 18:18:10 +0300

    chore: set paths alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,21 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-          '/app': {
-            target: 'http://localhost:5001',
-            channgeOrigin: true,
-            secure: false,
-          },
-        },
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/app': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
     },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
