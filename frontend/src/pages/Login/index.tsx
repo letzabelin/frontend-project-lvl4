aff2d6e4756a6d73998434e8157168ef8409e895
Andrew Zabelin <letzabelin@gmail.com>
2023-03-03 21:12:43 +0300

    feat: add formik & yup to login page

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,16 +1,22 @@
 import { Link } from 'react-router-dom';
-import {
-  Button,
-  Card,
-  Col,
-  Container,
-  FloatingLabel,
-  Form,
-  Image,
-} from 'react-bootstrap';
+import { Button, Card, Col, Container, FloatingLabel, Form, Image } from 'react-bootstrap';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 import profileImage from '@/assets/images/profile.png';
 
 const LoginPage = (): JSX.Element => {
+  const loginScheme = yup.object({
+    username: yup.string().trim().required().default(''),
+    password: yup.string().trim().required().default(''),
+  });
+
+  const formik = useFormik({
+    initialValues: loginScheme.getDefault(),
+    validationSchema: loginScheme,
+
+    onSubmit: console.log,
+  });
+
   return (
     <Container className="vh-100 d-flex justify-content-center align-items-center">
       <Card className="w-50">
@@ -22,16 +28,16 @@ const LoginPage = (): JSX.Element => {
           <Col>
             <h1 className="text-center mb-3">Войти</h1>
 
-            <Form>
+            <Form onSubmit={formik.handleSubmit}>
               <Form.Group className="mb-3">
                 <FloatingLabel label="Ваш ник">
-                  <Form.Control type="text" placeholder="name@example.com" required />
+                  <Form.Control type="text" placeholder="name@example.com" required {...formik.getFieldProps('username')} />
                 </FloatingLabel>
               </Form.Group>
 
               <Form.Group className="mb-3">
                 <FloatingLabel label="Пароль">
-                  <Form.Control type="password" placeholder="Password" required />
+                  <Form.Control type="password" placeholder="Password" required {...formik.getFieldProps('password')} />
                 </FloatingLabel>
               </Form.Group>
 
@@ -44,7 +50,7 @@ const LoginPage = (): JSX.Element => {
 
         <Card.Footer className="p-4 text-center">
           <span>Нет аккаунта?&nbsp;</span>
-          <Link to="/">Регистрация</Link>
+          <Link to="/signup">Регистрация</Link>
         </Card.Footer>
       </Card>
     </Container>
